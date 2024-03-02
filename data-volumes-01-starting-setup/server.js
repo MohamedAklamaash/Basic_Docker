3e9fcf65e5dab8f3632b9b4587a3fca21c4969d9
MohamedAklamaash <aklamaash78@gmail.com>
2024-03-02 11:01:35 +0530

    session 5 done

--- data-volumes-01-starting-setup/server.js
+++ data-volumes-01-starting-setup/server.js
@@ -43,6 +43,6 @@ app.post('/create', async (req, res) => {
   });
 }); 
 
-app.listen(8001,()=>{
-  console.log("Server is running on port 8001");
+app.listen(process.env.PORT,()=>{
+  console.log(`Server is running on port 8001 ${process.env.PORT}`);
 });
